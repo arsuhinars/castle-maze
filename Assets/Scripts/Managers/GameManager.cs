d26b0e5025c0c35e6f7533f7bda4898e5bf228eb
arsuhinars <arsuhinars@gmail.com>
2023-02-24 13:05:39 +0400

    ✨ Implemented GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; } = null;
+
+    public event Action OnStart;
+    public event Action OnPlayerDeath;
+    public event Action OnPause;
+    public event Action OnResume;
+
+    public bool IsStarted { get; set; }
+    public bool IsPaused { get; set; }
+
+    public PlayerEntity PlayerEntity => m_playerEntity;
+    public CameraController CameraController => m_cameraController;
+
+    private PlayerEntity m_playerEntity = null;
+    private CameraController m_cameraController = null;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+
+        m_playerEntity = FindObjectOfType<PlayerEntity>();
+        m_cameraController = FindObjectOfType<CameraController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Start()
     {
-        
+        // Пропускаем один кадр для того, чтобы все
+        // объекты успели подписаться на события
+        yield return null;
+
+        OnStart?.Invoke();
     }
 }
