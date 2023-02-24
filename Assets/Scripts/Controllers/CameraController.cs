4b9bfff5184602a9fa18b2956569174068e4f4e9
arsuhinars <arsuhinars@gmail.com>
2023-02-24 13:05:07 +0400

    ✨ Implemented CameraController

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,18 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera TargetCamera
     {
-        
+        get => m_targetCamera;
+        set => m_targetCamera = value;
+    }
+    
+    /// <summary>
+    /// Объект настроек камеры. Обратите внимание, что для применения изменений нужно
+    /// заново присвоить объект.
+    /// </summary>
+    public CameraSettings Settings
+    {
+        get => m_settings;
+        set
+        {
+            m_settings = value;
+            ApplySettings();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 TargetPoint
     {
-        
+        get => m_targetPoint;
+        set => m_targetPoint = value;
+    }
+
+    [SerializeField] private Camera m_targetCamera;
+    [SerializeField] private CameraSettings m_settings;
+    
+    private Vector3 m_normalizedLookDir = Vector3.forward;
+    private Vector3 m_targetPoint = Vector3.zero;
+    private Vector3 m_velocity = Vector3.zero;
+
+    /// <summary>
+    /// Метод для перемещения камеры к <c>TargetPoint</c> мгновенно, без плавного перехода.
+    /// </summary>
+    public void MoveInstantly()
+    {
+        m_targetCamera.transform.position = m_targetPoint;
+        m_velocity = Vector3.zero;
+    }
+    
+    private void Start()
+    {
+        ApplySettings();
+    }
+
+    private void Update()
+    {
+        var cameraPosition = m_targetPoint - 
+            m_normalizedLookDir * m_settings.lookDistance;
+
+        // Плавно перемещаем камеру к заданной точке
+        m_targetCamera.transform.position = Vector3.SmoothDamp(
+            m_targetCamera.transform.position,
+            cameraPosition,
+            ref m_velocity,
+            m_settings.moveSmoothTime,
+            m_settings.moveMaxSpeed
+        );
+    }
+
+    private void ApplySettings()
+    {
+        m_normalizedLookDir = m_settings.lookDirection.normalized;
+
+        m_targetCamera.transform.rotation = Quaternion.LookRotation(
+            m_normalizedLookDir
+        );
     }
 }
