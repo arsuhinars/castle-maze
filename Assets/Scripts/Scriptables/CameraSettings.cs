2b20b05e0787cca70ac4ad3975c92cfde4449909
arsuhinars <arsuhinars@gmail.com>
2023-02-24 13:05:22 +0400

    ✨ Added CameraSettings scriptable

---
+++ Assets/Scripts/Scriptables/CameraSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CameraSettings", menuName = "Game/Camera Settings")]
+public class CameraSettings : ScriptableObject
+{
+    public float moveSmoothTime = 1.0f;
+    public float moveMaxSpeed = 1.0f;
+    public Vector3 lookDirection = Vector3.forward;
+    public float lookDistance = 1.0f;
+}
