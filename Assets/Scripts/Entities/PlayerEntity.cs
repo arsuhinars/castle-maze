cb05bde767e7c8896eaf35521e252536374c3e8f
arsuhinars <arsuhinars@gmail.com>
2023-02-24 13:06:53 +0400

    ✨ Implemented PlayerEntity

--- Assets/Scripts/Entities/PlayerEntity.cs
+++ Assets/Scripts/Entities/PlayerEntity.cs
@@ -1,8 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerEntity : CreatureEntity
 {
+    private void Start()
+    {
+        GameManager.Instance.OnStart += OnGameStart;
+    }
 
+    private void OnGameStart()
+    {
+        GameManager.Instance.CameraController.TargetPoint = transform.position;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // Двигаем камеру к игроку
+        var camController = GameManager.Instance.CameraController;
+        var camTarget = new Vector3(
+            transform.position.x,
+            camController.TargetPoint.y,
+            transform.position.z
+        );
+        camController.TargetPoint = camTarget;
+    }
 }
