64414396b37ab6d20fe3df74ba7e38895c85ed62
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:15:53 +0400

    ✨ Added PickupSettings class

---
+++ Assets/Scripts/Scriptables/PickupSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PickupSettings", menuName = "Game/Pickup Settings")]
+public class PickupSettings : ScriptableObject
+{
+    public string targetTag = string.Empty;
+    public bool disableOnPickup = true;
+    [Header("Animation settings")]
+    public float rotationTime = 1.0f;
+    public float moveAmplitude = 1.0f;
+    public float moveTime = 1.0f;
+}
