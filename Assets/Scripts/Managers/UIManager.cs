c4d6dd1096343fcdc922c654bf2967bfcbe8b89d
arsuhinars <arsuhinars@gmail.com>
2023-02-25 18:40:13 +0400

    ✨ Added UIManager class

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; } = null;
+
+    [SerializeField] private UIView[] m_views;
+
+    private UIView m_activeView;
+    private Dictionary<string, UIView> m_viewsByName;
+
+    public void ToggleView(string name)
+    {
+        if (m_activeView != null)
+        {
+            m_activeView.Hide();
+        }
+
+        m_activeView = m_viewsByName[name];
+        m_activeView.Show();
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        m_viewsByName = new();
+        foreach (var view in m_views)
+        {
+            view.gameObject.SetActive(true);
+            view.Hide();
+
+            m_viewsByName[view.ViewName] = view;
+        }
+    }
+}
