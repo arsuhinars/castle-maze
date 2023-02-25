2efb4d93f3fe02fc6d1630387450e4047981a7fd
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:12:52 +0400

    ✨ Implemented StageManager class

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static StageManager Instance { get; private set; } = null;
+
+    public StageController CurrentStage => m_stages[m_currStageIndex];
+
+    [SerializeField]
+    [Tooltip(
+        "Массив стадий текущего уровня. Стадии распологаются " +
+        "в порядке их прохождения игроком"
+    )]
+    private StageController[] m_stages;
+
+    private int m_currStageIndex = 0;
+
+    public void SetActiveStage(int index)
     {
+        // Отключаем объекты текущей стадии
+        m_stages[m_currStageIndex].IsActive = false;
         
+        // Включаем объекты новой
+        m_currStageIndex = index;
+        m_stages[m_currStageIndex].IsActive = true;
+        
+        // Включаем переход к следующей стадии
+        if (m_currStageIndex + 1 < m_stages.Length)
+        {
+            var transition = m_stages[m_currStageIndex + 1].TransitionRoot;
+            if (transition != null)
+            {
+                transition.SetActive(true);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        foreach (var stage in m_stages)
+        {
+            stage.IsActive = false;
+        }
+
+        SetActiveStage(m_currStageIndex);
     }
 }
