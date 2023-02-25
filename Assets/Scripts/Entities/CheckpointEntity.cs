54380c47a9fa2cd90b8663dfa154f696c6d88e14
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:12:14 +0400

    ✨ Implemented CheckpointEntity class

--- Assets/Scripts/Entities/CheckpointEntity.cs
+++ Assets/Scripts/Entities/CheckpointEntity.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckpointEntity : MonoBehaviour
+public class CheckpointEntity : PickupEntity
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform PlayerSpawn => m_playerSpawn;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private Transform m_playerSpawn;
 }
