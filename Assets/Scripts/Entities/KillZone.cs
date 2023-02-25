6ec489862360e9f7277f8321898d309e7a3547e6
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:15:42 +0400

    ✨ Added KillZone class

---
+++ Assets/Scripts/Entities/KillZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] private KillZoneSettings m_settings;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(m_settings.targetTag) &&
+            other.TryGetComponent<CreatureEntity>(out var creature))
+        {
+            creature.Kill();
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptables/KillZoneSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "KillZoneSettings", menuName = "Game/Kill Zone Settings")]
+public class KillZoneSettings : ScriptableObject
+{
+    public string targetTag = string.Empty;
+}
