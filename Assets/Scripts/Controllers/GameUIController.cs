2ea139c4bad21901e5f79ae130fab283dd21022a
arsuhinars <arsuhinars@gmail.com>
2023-02-25 18:42:11 +0400

    ✨ Added GameUIController class

---
+++ Assets/Scripts/Controllers/GameUIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    private void Start()
+    {
+        var gameManager = GameManager.Instance;
+
+        gameManager.OnStart += OnGameStart;
+        gameManager.OnEnd += OnGameEnd;
+        gameManager.OnPause += OnGamePause;
+        gameManager.OnResume += OnGameResume;
+    }
+
+    private void OnGameStart()
+    {
+        UIManager.Instance.ToggleView(Defines.IN_GAME_VIEW);
+    }
+
+    private void OnGameEnd()
+    {
+        UIManager.Instance.ToggleView(Defines.GAME_END_VIEW);
+    }
+
+    private void OnGamePause()
+    {
+        UIManager.Instance.ToggleView(Defines.PAUSE_VIEW);
+    }
+
+    private void OnGameResume()
+    {
+        UIManager.Instance.ToggleView(Defines.IN_GAME_VIEW);
+    }
+}
