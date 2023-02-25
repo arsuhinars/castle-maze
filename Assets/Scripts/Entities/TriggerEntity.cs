87f075062b84d6f814d6d716491134b5673091ae
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:16:05 +0400

    ✨ Added TriggerEntity class

---
+++ Assets/Scripts/Entities/TriggerEntity.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEntity : MonoBehaviour
+{
+    [SerializeField]
+    private TriggerSettings m_settings;
+    [SerializeField]
+    private UnityEvent m_onTriggerEnter = new();
+
+    private bool m_triggeredFlag = false;
+
+    private void Start()
+    {
+        GameManager.Instance.OnStart += OnGameStart;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.OnStart -= OnGameStart;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_triggeredFlag && m_settings.triggerOnce)
+        {
+            return;
+        }
+
+        if (other.CompareTag(m_settings.targetTag))
+        {
+            m_triggeredFlag = true;
+            m_onTriggerEnter.Invoke();
+        }
+    }
+
+    private void OnGameStart()
+    {
+        if (m_settings.resetOnGameRestart)
+        {
+            m_triggeredFlag = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptables/TriggerSettings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TriggerSettings", menuName = "Game/Trigger Settings")]
+public class TriggerSettings : ScriptableObject
+{
+    public string targetTag = string.Empty;
+    public bool triggerOnce = true;
+    public bool resetOnGameRestart = true;
+}
