7623a31e49fdc5155d200fa0c40c3225c912c720
arsuhinars <arsuhinars@gmail.com>
2023-02-25 23:28:54 +0400

    ✨ Added support for player lives in GameEndView

--- Assets/Scripts/UI/Views/GameEndView.cs
+++ Assets/Scripts/UI/Views/GameEndView.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +10,33 @@ public class GameEndView : UIView
     [SerializeField] private Button m_retryButton;
     [SerializeField] private Button m_restartButton;
     [SerializeField] private Button m_leaveButton;
+    [SerializeField] private TextMeshProUGUI m_livesCountText;
 
     private void Start()
     {
         m_retryButton.onClick.AddListener(
             () => GameManager.Instance.StartGame()
         );
-        // TODO
+        m_restartButton.onClick.AddListener(
+            () => GameManager.Instance.ReloadGame()
+        );
         m_leaveButton.onClick.AddListener(
             () => GameManager.Instance.LeaveGame()
         );
+    }
+
+    public override void Show()
+    {
+        // Создаем строку формата "x<КОЛ-ВО ЖИЗНЕЙ>"
+        var strBuilder = new StringBuilder(3);
+        strBuilder.Append('x');
+        strBuilder.Append(GameManager.Instance.PlayerLives);
+        
+        m_livesCountText.text = strBuilder.ToString();
+
+        m_retryButton.interactable = GameManager.Instance.PlayerLives > 0;
+        m_leaveButton.gameObject.SetActive(!m_retryButton.interactable);
+
+        base.Show();
     }
 }
