986a8482447a7b463c3909fe6cfd9eb39843f2da
arsuhinars <arsuhinars@gmail.com>
2023-02-25 18:39:52 +0400

    ✨ Added UIView class

---
+++ Assets/Scripts/UI/UIView.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(CanvasGroup))]
+public abstract class UIView : MonoBehaviour
+{
+    public abstract string ViewName { get; }
+    public Canvas Canvas => m_canvas;
+    public CanvasGroup CanvasGroup => m_canvasGroup;
+    public bool IsShowed => m_isShowed;
+
+    private Canvas m_canvas;
+    private CanvasGroup m_canvasGroup;
+    private bool m_isShowed = false;
+
+    public void Show()
+    {
+        m_canvas.enabled = true;
+        m_canvasGroup.blocksRaycasts = true;
+        m_isShowed = true;
+    }
+
+    public void Hide()
+    {
+        m_canvas.enabled = false;
+        m_canvasGroup.blocksRaycasts = false;
+        m_isShowed = false;
+    }
+
+    private void Awake()
+    {
+        m_canvas = GetComponent<Canvas>();
+        m_canvasGroup = GetComponent<CanvasGroup>();
+    }
+}
