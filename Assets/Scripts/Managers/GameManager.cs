091be842d245fd4caf25db91b3dc2120c8517c3c
arsuhinars <arsuhinars@gmail.com>
2023-02-25 14:13:16 +0400

    ✨ GameManager class update

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,18 +7,52 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; } = null;
 
     public event Action OnStart;
-    public event Action OnPlayerDeath;
+    public event Action OnEnd;
     public event Action OnPause;
     public event Action OnResume;
 
-    public bool IsStarted { get; set; }
-    public bool IsPaused { get; set; }
+    public bool IsStarted
+    {
+        get => m_isStarted;
+        set
+        {
+            if (!m_isStarted && value)
+            {
+                HandleStart();
+            }
+            else if (m_isStarted && !value)
+            {
+                HandleEnd();
+            }
+
+            m_isStarted = value;
+        }
+    }
+    public bool IsPaused
+    {
+        get => m_isPaused;
+        set
+        {
+            if (!m_isPaused && value)
+            {
+                HandlePause();
+            }
+            else if (m_isPaused && !value)
+            {
+                HandleResume();
+            }
+
+            m_isPaused = value;
+        }
+    }
 
     public PlayerEntity PlayerEntity => m_playerEntity;
     public CameraController CameraController => m_cameraController;
 
     private PlayerEntity m_playerEntity = null;
     private CameraController m_cameraController = null;
+    private bool m_isStarted = false;
+    private bool m_isPaused = false;
 
     private void Awake()
     {
@@ -38,6 +72,30 @@ public class GameManager : MonoBehaviour
         // объекты успели подписаться на события
         yield return null;
 
+        m_isStarted = true;
+        HandleStart();
+    }
+
+    private void HandleStart()
+    {
+        IsPaused = false;
         OnStart?.Invoke();
+    }
+
+    private void HandleEnd()
+    {
+        OnEnd?.Invoke();
+    }
+
+    private void HandlePause()
+    {
+        Time.timeScale = 0.0f;
+        OnPause?.Invoke();
+    }
+
+    private void HandleResume()
+    {
+        Time.timeScale = 1.0f;
+        OnResume?.Invoke();
     }
 }
