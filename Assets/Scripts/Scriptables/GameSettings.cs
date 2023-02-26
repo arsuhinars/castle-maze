294a0d1ba3ae4c14c5b50b173bebaedb40c67075
arsuhinars <arsuhinars@gmail.com>
2023-02-26 23:58:06 +0400

    ✨ Added playerAbilities field in GameSettings

--- Assets/Scripts/Scriptables/GameSettings.cs
+++ Assets/Scripts/Scriptables/GameSettings.cs
@@ -1,8 +1,8 @@
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameSettings", menuName = "Game/Game Settings")]
 public class GameSettings : ScriptableObject
 {
     public int initialLivesCount = 1;
+    public PlayerAbility[] playerAbilities;
 }
