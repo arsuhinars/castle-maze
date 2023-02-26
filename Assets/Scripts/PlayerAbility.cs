7066fc9d1a798ae791b04debb01ae4b93a0c95ee
arsuhinars <arsuhinars@gmail.com>
2023-02-26 23:55:23 +0400

    ✨ Added PlayerAbility class

---
+++ Assets/Scripts/PlayerAbility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerAbility
+{
+    public string name;
+    public string displayName;
+    public GameObject prefab;
+    public Sprite icon;
+}
