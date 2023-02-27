f02db8aba45bd861ada7a7ed8b637dee30195edc
arsuhinars <arsuhinars@gmail.com>
2023-02-28 00:09:09 +0400

    ✨ Added AbilityPickup class

---
+++ Assets/Scripts/Entities/AbilityPickup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AbilityPickup : PickupEntity
+{
+    [SerializeField] private string m_abilityName;
+
+    protected override void HandlePickup()
+    {
+        GameManager.Instance.PlayerEntity.AddAbility(m_abilityName);
+    }
+}
