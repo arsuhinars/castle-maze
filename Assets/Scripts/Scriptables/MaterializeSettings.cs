fca5f033cb16ea19d7f574b21de7c293152bd38e
arsuhinars <arsuhinars@gmail.com>
2023-03-01 10:24:37 +0400

    ✨ Added MaterializeSettings class

---
+++ Assets/Scripts/Scriptables/MaterializeSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "MaterializeSettings",
+    menuName = "Game/Abilities/Materialize Settings"
+)]
+public class MaterializeSettings : ScriptableObject
+{
+    public LayerMask groundMask;
+    public Collider bridgePrefab;
+    public float bridgeMoveTime = 0f;
+}
