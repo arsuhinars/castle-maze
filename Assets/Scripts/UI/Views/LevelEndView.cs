26e5e362467944ad29487fbd030d10f0ffec568f
arsuhinars <arsuhinars@gmail.com>
2023-03-01 10:22:49 +0400

    ✨ Added LevelEndView

---
+++ Assets/Scripts/UI/Views/LevelEndView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelEndView : UIView
+{
+    public override string ViewName => Defines.LEVEL_END_VIEW;
+
+    [SerializeField] private Button m_nextLevelButton;
+    [SerializeField] private Button m_leaveButton;
+
+    private void Start()
+    {
+        m_nextLevelButton.onClick.AddListener(
+            () => GameManager.Instance.ChangeLevel(
+                ProgressManager.Instance.CurrentLevel + 1
+            )
+        );
+        m_leaveButton.onClick.AddListener(
+            () => GameManager.Instance.LeaveGame()
+        );
+    }
+
+    public override void Show()
+    {
+        base.Show();
+
+        var currLevel = ProgressManager.Instance.CurrentLevel;
+        var levelCount = GameManager.Instance.Settings.levelsScenes.Length;
+
+        m_nextLevelButton.interactable = currLevel < levelCount;
+    }
+}
--- Assets/Scripts/Utils/Defines.cs
+++ Assets/Scripts/Utils/Defines.cs
@@ -4,4 +4,5 @@ public static class Defines
     public const string IN_GAME_VIEW = "InGame";
     public const string PAUSE_VIEW = "Pause";
     public const string GAME_END_VIEW = "GameEnd";
+    public const string LEVEL_END_VIEW = "LevelEnd";
 }
